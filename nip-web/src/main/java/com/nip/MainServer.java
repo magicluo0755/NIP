e0cd01dd9a451e0708a14042dd7cb2427df90c3e
Laurence <xqluo2004@163.com>
2025-03-17 15:11:29 +0800

    初始化代码工程

---
+++ nip-web/src/main/java/com/nip/MainServer.java
@@ -0,0 +1,12 @@
+package com.nip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainServer {
+    public static void main(String[] args) {
+
+        SpringApplication.run(Main.class, args);
+    }
+}
---
+++ src/main/java/com/nip/Main.java
@@ -0,0 +1,7 @@
+package com.nip;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}

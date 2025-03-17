eb618bc14d900a02dadb30a3dba5e2b24c4bf658
Laurence <xqluo2004@163.com>
2025-03-17 17:30:25 +0800

    更新不要的工程文件

--- nip-assembly/src/main/java/com/nip/Main.java
+++
@@ -1,7 +0,0 @@
-package com.nip;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ src/main/resources/bin/start.bat
@@ -0,0 +1,3 @@
+@echo off
+:: 启动 NIP 引擎服务端
+java -jar lib/nip-web-1.0.0-SNAPSHOT.jar
